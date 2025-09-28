88c1d14fdabbe612f67f120a67da96ad665afaec
DaniyarAbS <daniar.absat@bk.ru>
2025-09-28 21:10:53 +0500

    init: maven, junit5, ci, readme

---
+++ test/java/ExampleTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExampleTest {
+    @Test
+    void additionWorks() {
+        assertEquals(2, 1 + 1);
+    }
+}
